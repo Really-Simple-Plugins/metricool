0dfc483312b05b52615e2940af6b1e759906f1b4
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-10 12:12:34 +0200

    feat: created TabNavigation for block header

---
+++ react/src/custom/TabNavigation.tsx
@@ -0,0 +1,23 @@
+import type { Dispatch, SetStateAction } from "react";
+import FlexContainer from "./FlexContainer.tsx";
+import { clsx } from "clsx";
+
+type TabNavigationProps = {
+    tabs: string[],
+    separator?: boolean,
+    activeTab: number,
+    onTabClick: Dispatch<SetStateAction<number>> | ((index: number) => void),
+}
+
+const TabNavigation = ({ tabs, activeTab, onTabClick, separator = false }: TabNavigationProps) => {
+    return (
+        <FlexContainer direction={"row"} className={"w-auto leading-none text-sm !gap-2"}>
+            {tabs.map((tab, index) => (<>
+                <span onClick={() => onTabClick(index)} className={clsx("cursor-pointer", activeTab === index ? "font-semibold" : "text-gray-600")}>{tab}</span>
+                {separator && index != tabs.length - 1 && <span>|</span>}
+            </>))}
+        </FlexContainer>
+    );
+};
+
+export default TabNavigation;
