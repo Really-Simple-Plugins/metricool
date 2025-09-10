ef3ac17273c566c939fe3dc54920407222eb23c6
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-10 12:20:42 +0200

    feat: implemented full Dashboard layout with all blocks

--- react/src/layouts/DashboardLayout.tsx
+++ react/src/layouts/DashboardLayout.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { __ } from '@wordpress/i18n';
+import Header from "../custom/Header.tsx";
+import FlexContainer from "../custom/FlexContainer.tsx";
+import Progress from "../custom/Progress.tsx";
+import WebsiteAnalytics from "../custom/WebsiteAnalytics.tsx";
+import ConnectedAccounts from "../custom/ConnectedAccounts.tsx";
+import OtherPlugins from "../custom/OtherPlugins.tsx";
 
-export const DashboardLayout = ({ children }: { children?: React.ReactNode }) => {
+export const DashboardLayout = () => {
     return (
-        <div>
-            <div className='p-2 flex gap-2'>
-                {__('Dashboard', 'metricool')}
-            </div>
-            <hr/>
-            {children}
-        </div>
+        <FlexContainer direction={"column"} className={"h-full w-full"}>
+            <Header/>
+            <FlexContainer direction={"column"} className={"px-4 w-full"}>
+                <FlexContainer direction={"column"} className={"w-full justify-around md:flex-row"}>
+                    <Progress />
+                    <WebsiteAnalytics />
+                </FlexContainer>
+                <FlexContainer direction={"column"} className={"w-full justify-around sm:flex-row"}>
+                    <ConnectedAccounts />
+                    <OtherPlugins />
+                </FlexContainer>
+            </FlexContainer>
+        </FlexContainer>
     );
 };
