9981f5f82df850d446af9c9a91bc0f9e6b4cff98
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-10 12:09:10 +0200

    feat: created Dashboard header

---
+++ react/src/custom/Header.tsx
@@ -0,0 +1,32 @@
+import { __ } from '@wordpress/i18n';
+import { useGlobalContext } from "../context/GlobalContext.tsx";
+import { Button } from "../components";
+import HeaderTab from "./HeaderTab.tsx";
+
+const Header = () => {
+    const { globalState } = useGlobalContext();
+    return (
+        <div className="bg-white min-w-full mx-auto px-4 flex justify-between items-center">
+            <div className={"flex gap-8"}>
+                <img src={`${globalState.metricool.assets_url}img/mc-logo.svg`} alt="Metricool logo"/>
+                <div className="flex gap-4">
+                    <HeaderTab link="/">
+                        {__('Dashboard', 'metricool')}
+                    </HeaderTab>
+                    <HeaderTab link="/settings">
+                        {__('Settings', 'metricool')}
+                    </HeaderTab>
+                    <HeaderTab link="https://www.metricool.com" external={true}>
+                        {__('Planner', 'metricool')}
+                    </HeaderTab>
+                </div>
+            </div>
+            <div className={"flex gap-4"}>
+                <Button variant={"black"} icon={"faq"} iconPosition={"left"} iconClass={"text-white"}>{__("Help Center", "metricool")}</Button>
+                <Button variant={"primary-gradient"} icon={"sparkle"} iconPosition={"left"} iconClass={"text-secondary"}>{__("Upgrade to Premium", "metricool")}</Button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ react/src/custom/HeaderTab.tsx
@@ -0,0 +1,17 @@
+import { Link } from "@tanstack/react-router";
+import { Icon } from "../components";
+
+const HeaderTab = ({ link, children, external = false }: {
+    link: string,
+    children: React.ReactNode,
+    external?: boolean
+}) => {
+    return (
+        <Link to={link} {...(external && { target: "_blank" })} className="text-md text-black items-center flex gap-1 py-[23px] focus:outline-hidden relative ease-in-out duration-100 [&.active]:border-b-2 [&.active]:border-b-primary hover:border-b-2 hover:border-b-primary-light hover:text-black">
+            {children}
+            {external && (<Icon icon={"external-link"}/>)}
+        </Link>
+    );
+};
+
+export default HeaderTab;
