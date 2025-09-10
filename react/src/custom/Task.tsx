db3f090da9aab48ebb38cfc3eaf4a68db56f0d4e
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-10 12:12:59 +0200

    feat: created Task component

---
+++ react/src/custom/Task.tsx
@@ -0,0 +1,36 @@
+import FlexContainer from "./FlexContainer.tsx";
+import { Badge, Button } from "../components";
+import { capitalizeFirstCharacter } from "../functions/utils.tsx";
+
+export type TaskProps = {
+    status: "warning" | "open" | "premium",
+    text: string,
+    id: number,
+    action: {
+        text: string,
+        link: string
+    },
+    completed: boolean,
+    dismissed: boolean,
+}
+
+const Task = ({ task: { status, text, action, completed, dismissed } }: { task: TaskProps }) => {
+    return (
+        <FlexContainer direction={"row"} className={"justify-between"}>
+            <FlexContainer direction={"row"} className={"items-center"}>
+                <Badge variant={status}>{capitalizeFirstCharacter(status)}</Badge>
+                <div className={"font-semibold"}>{text}</div>
+            </FlexContainer>
+            <FlexContainer direction={"row"} className={"items-center"}>
+                <div className={"underline cursor-pointer"}><span className={"text-nowrap"}>{action.text}</span></div>
+                {!(completed || dismissed) ? (
+                    <Button variant={"icon"} icon={"close"} iconPosition={"right"}/>
+                ) : (
+                    <div className={"w-4 h-4"}></div>
+                )}
+            </FlexContainer>
+        </FlexContainer>
+    );
+};
+
+export default Task;
