627a14bf08961dcaec1a9fbd4fba489dcc06ede2
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-10 09:31:01 +0200

    fix: turned off sourcemap generation

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         outDir: './build',
         emptyOutDir: true,
         manifest: true,
-        sourcemap: true,
+        sourcemap: false,
         rollupOptions: {
             input: './src/main.tsx',
         },
