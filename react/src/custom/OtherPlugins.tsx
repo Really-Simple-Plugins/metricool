84d24ed4788ed293b454c7d3f4cfb3ce87011abc
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-10 12:15:46 +0200

    feat: created OtherPlugins block

---
+++ react/src/custom/OtherPlugins.tsx
@@ -0,0 +1,27 @@
+import { Card, CardHeader, CardHeaderTitle } from "../components";
+import { __ } from "@wordpress/i18n";
+import FlexContainer from "./FlexContainer.tsx";
+import ListItem from "./ListItem.tsx";
+
+const OtherPlugins = () => {
+    return (
+        <Card variant={"transparent"}>
+            <CardHeader>
+                <CardHeaderTitle>{__("Other Plugins", "metricool")}</CardHeaderTitle>
+            </CardHeader>
+            <FlexContainer direction={"column"} className={"!gap-2"}>
+                <ListItem icon={"circle"} iconColor={"rss"} link={__("Upgrade", "metricool")} className={"font-semibold"}>
+                    {__("Really Simple Security Pro", "metricool")}
+                </ListItem>
+                <ListItem icon={"circle"} iconColor={"simplybook"} link={__("Install", "metricool")} className={"font-semibold"}>
+                    {__("Simplybook: Online booking system", "metricool")}
+                </ListItem>
+                <ListItem icon={"circle"} iconColor={"complianz"} link={__("Install", "metricool")} className={"font-semibold"}>
+                    {__("Complianz - Terms and Conditions", "metricool")}
+                </ListItem>
+            </FlexContainer>
+        </Card>
+    );
+};
+
+export default OtherPlugins;
