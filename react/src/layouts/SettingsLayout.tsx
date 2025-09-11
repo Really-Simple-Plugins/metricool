9a1ee90738474f518d8fa5c2ec8015c336245707
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-11 09:26:58 +0200

    feat: implemented basic settings menu list

--- react/src/layouts/SettingsLayout.tsx
+++ react/src/layouts/SettingsLayout.tsx
@@ -2,6 +2,7 @@ import FlexContainer from "../custom/FlexContainer.tsx";
 import Header from "../custom/Header.tsx";
 import { Card, CardHeader, CardHeaderTitle } from "../components";
 import { __ } from "@wordpress/i18n";
+import ListItem from "../custom/ListItem.tsx";
 
 export const SettingsLayout = () => {
     return (
@@ -14,6 +15,13 @@ export const SettingsLayout = () => {
                             {__("Settings", "metricool")}
                         </CardHeaderTitle>
                     </CardHeader>
+                    <FlexContainer direction={"column"} className={"!gap-3"}>
+                        <ListItem className={"text-md text-primary cursor-pointer hover:underline font-semibold"}>{__("Account Settings", "metricool")}</ListItem>
+                        <ListItem className={"text-md cursor-pointer hover:underline"}>{__("Connections", "metricool")}</ListItem>
+                        <ListItem className={"text-md cursor-pointer hover:underline"} icon={"external-link"} iconPosition={"right"}>{__("Affiliation Program", "metricool")}</ListItem>
+                        <ListItem className={"text-md text-upsell font-semibold cursor-pointer hover:underline"} icon={"pro"} iconClass={"rounded-full bg-(--color-upsell) size-2 p-0.5"} iconPosition={"right"}>{__("User Management", "metricool")}</ListItem>
+                        <ListItem className={"text-md text-upsell font-semibold cursor-pointer hover:underline"} icon={"pro"} iconClass={"rounded-full bg-(--color-upsell) size-2 p-0.5"} iconPosition={"right"}>{__("My Tasks", "metricool")}</ListItem>
+                    </FlexContainer>
                 </Card>
                 <FlexContainer direction={"column"} className={"md:min-w-[50%]"}>
                     <Card>
