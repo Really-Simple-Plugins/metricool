b9eeb9066a822c1bcc59604ef9de6f42b4871a6a
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-11 11:52:14 +0200

    feat: added scroll percentage utility function

--- react/src/functions/utils.tsx
+++ react/src/functions/utils.tsx
@@ -1,3 +1,11 @@
 export const capitalizeFirstCharacter = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
+
+export const getScrollProgressPercent = () => {
+    const totalScrollableHeightInPixels =
+        document.documentElement.scrollHeight - window.innerHeight;
+    const roundedScrollPercentage =
+        Math.round((window.scrollY / totalScrollableHeightInPixels) * 100);
+    return roundedScrollPercentage;
+};
