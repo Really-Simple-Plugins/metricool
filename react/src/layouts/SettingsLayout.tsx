9df9cc86d0acb1e8163c99d761d091f05b680c16
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-18 10:55:32 +0200

    feat: made SettingsLayout display the child outlet

--- react/src/layouts/SettingsLayout.tsx
+++ react/src/layouts/SettingsLayout.tsx
@@ -1,16 +1,15 @@
 import FlexContainer from "../custom/FlexContainer.tsx";
 import Header from "../custom/Header.tsx";
-import AccountSettings from "../custom/AccountSettings.tsx";
 import SettingsMenu from "../custom/SettingsMenu.tsx";
 import NotificationsSidebar from "../custom/NotificationsSidebar.tsx";
 
-export const SettingsLayout = () => {
+export const SettingsLayout = ({ children }: React.ComponentProps<"div">) => {
     return (
         <FlexContainer direction={"column"} className={"h-full w-full"}>
             <Header />
             <FlexContainer direction={"column"} className={"px-4 w-full justify-between md:flex-row items-start"}>
                 <SettingsMenu />
-                <AccountSettings />
+                {children}
                 <NotificationsSidebar />
             </FlexContainer>
         </FlexContainer>
