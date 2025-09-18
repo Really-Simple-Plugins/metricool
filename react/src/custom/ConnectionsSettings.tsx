b597efb3c3c120959c5d2165b289e8009c812391
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-18 17:47:43 +0200

    feat: filled out content of Connections Settings

--- react/src/custom/ConnectionsSettings.tsx
+++ react/src/custom/ConnectionsSettings.tsx
@@ -1,17 +1,32 @@
-import { Card, CardHeader, CardHeaderTitle } from "../components";
+import { Button, Card, CardHeader, CardHeaderTitle, CardDescription } from "../components";
 import { __ } from "@wordpress/i18n";
 import FlexContainer from "./FlexContainer.tsx";
+import AccountTile from "./AccountTile.tsx";
 
 const ConnectionsSettings = () => {
     return (
         <div className={"flex flex-col md:min-w-[50%]"}>
             <FlexContainer direction={"column"}>
-                <Card className={"min-h-[400px]"}>
+                <Card>
                     <CardHeader>
                         <CardHeaderTitle>
                             {__("Connections", "metricool")}
                         </CardHeaderTitle>
+                        <CardDescription>
+                            {__("The accounts that are connected to Metricool", "metricool")}
+                        </CardDescription>
                     </CardHeader>
+                    <FlexContainer direction={"column"}>
+                        <AccountTile connected={true} accountType={"domain"} accountName={"yourwebsite.com"}/>
+                        <AccountTile connected={true} accountType={"youtube"} accountName={"YourChannelName"}/>
+                        <AccountTile connected={false} accountType={"twitter"}/>
+                        <AccountTile connected={false} accountType={"linkedIn"}/>
+                    </FlexContainer>
+                    <FlexContainer direction={"row"} className={"justify-end"}>
+                        <Button variant={"primary-gradient-ghost"} icon={"external-link"} iconPosition={"right"} iconClass={"svg-gradient"} className={"size-fit"}>
+                            {__("Connected Accounts", "metricool")}
+                        </Button>
+                    </FlexContainer>
                 </Card>
             </FlexContainer>
         </div>
