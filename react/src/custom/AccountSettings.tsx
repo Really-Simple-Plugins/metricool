88c5c7f6602f2b9dc4641b3927bae4ad27ee924d
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-09-22 07:45:04 +0200

    feat: added form support with react-hook-form and zod

--- react/src/custom/AccountSettings.tsx
+++ react/src/custom/AccountSettings.tsx
@@ -2,10 +2,44 @@ import { Card, CardHeader, CardHeaderTitle } from "../components";
 import { __ } from "@wordpress/i18n";
 import FlexContainer from "./FlexContainer.tsx";
 import FormFooter from "./FormFooter.tsx";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    language: z.literal(["english"]),
+    timeZone: z.literal(["Europe/Amsterdam"]),
+    weekStart: z.literal(["monday", "sunday"]),
+    receiveMonthlySummary: z.boolean(),
+    customEmail: z.literal(["english"]),
+}).required();
 
 const AccountSettings = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+            language: "english",
+            timeZone: "Europe/Amsterdam",
+            weekStart: "sunday",
+            receiveMonthlySummary: false,
+            customEmail: "english",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
+
     return (
-        <div className={"flex flex-col md:min-w-[50%]"}>
+        <form onSubmit={handleSubmit(onSubmit)} className={"flex flex-col md:min-w-[50%]"}>
             <FlexContainer direction={"column"}>
                 <Card>
                     <CardHeader>
@@ -30,7 +64,7 @@ const AccountSettings = () => {
                 </Card>
             </FlexContainer>
             <FormFooter/>
-        </div>
+        </form>
     );
 };
 
