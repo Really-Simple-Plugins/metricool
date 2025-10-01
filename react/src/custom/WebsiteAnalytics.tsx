a9a7dbfb037dfe85f301022e6d9a8d3c9c50fb31
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-10-01 21:44:52 +0200

    feat: implemented dedicated tabs

--- react/src/custom/WebsiteAnalytics.tsx
+++ react/src/custom/WebsiteAnalytics.tsx
@@ -1,42 +1,35 @@
 import { useState } from "react";
-import { Button, Card, CardHeader, CardHeaderTitle } from "../components";
+import { Card, CardHeader, CardHeaderTitle } from "../components";
 import { __ } from "@wordpress/i18n";
 import TabNavigation from "./TabNavigation.tsx";
-import FlexContainer from "./FlexContainer.tsx";
+import CountriesTab from "./CountriesTab.tsx";
+import RealtimeTab from "./RealtimeTab.tsx";
+import AnalyticsTab from "./AnalyticsTab.tsx";
+import TrafficTab from "./TrafficTab.tsx";
 
 const WebsiteAnalytics = () => {
     const [activeTab, setActiveTab] = useState(1);
-    const tabs = [__("Real-time", "metricool"), __("Analytics", "metricool"), __("Traffic", "metricool"), __("Countries", "metricool")];
+    const tabs = [{
+        title: __("Real-time", "metricool"),
+        component: <RealtimeTab />
+    }, {
+        title: __("Analytics", "metricool"),
+        component: <AnalyticsTab />
+    }, {
+        title: __("Traffic", "metricool"),
+        component: <TrafficTab />
+    }, {
+        title: __("Countries", "metricool"),
+        component: <CountriesTab />
+    }];
+
     return (
-        <Card className={"justify-between"}>
+        <Card>
             <CardHeader className={"flex justify-between"}>
                 <CardHeaderTitle>{__("Website Analytics", "metricool")}</CardHeaderTitle>
                 <TabNavigation activeTab={activeTab} onTabClick={setActiveTab} tabs={tabs}></TabNavigation>
             </CardHeader>
-            <FlexContainer direction={"column"} className={"h-[290px] rounded-md bg-gray-50"}>
-                <FlexContainer direction={"row"}>
-                    <div className={"text-base font-semibold p-2"}>Website</div>
-                </FlexContainer>
-                <FlexContainer direction={"row"} className={"items-center justify-center h-[100px]"}>
-                    CHART
-                </FlexContainer>
-            </FlexContainer>
-            <FlexContainer direction={"row"} className={"justify-between items-center"}>
-                <FlexContainer direction={"row"} className={"sm:flex-col xl:flex-row"}>
-                    <Button variant={"upsell"} icon={"down"} iconPosition={"left"}>
-                        {__("Last 30 Days", "metricool")}
-                    </Button>
-                    <Button variant={"upsell"} icon={"file"} iconPosition={"left"}>
-                        {__("Generate Report", "metricool")}
-                    </Button>
-                    <Button variant={"upsell"} icon={"download"} iconPosition={"left"}>
-                        {__("Download CSV", "metricool")}
-                    </Button>
-                </FlexContainer>
-                <Button variant={"primary-gradient-ghost"} icon={"external-link"} iconPosition={"right"} iconClass={"svg-gradient"}>
-                    {__("View Analytics", "metricool")}
-                </Button>
-            </FlexContainer>
+            {tabs[activeTab].component}
         </Card>
     );
 };
