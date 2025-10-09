db5a5446372559acbc7a89bc351cf4eaf238749c
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-10-09 08:50:34 +0200

    feat: added questions for wim

--- app/http/metricool/Entities/Models/Statistic.php
+++ app/http/metricool/Entities/Models/Statistic.php
@@ -4,13 +4,14 @@ namespace Metricool\Http\Metricool\Entities\Models;
 
 use Carbon\Carbon;
 
-class Statistic // <- name too generic?
+class Statistic // ASK -> name too generic?
 {
     public Carbon $date;
     public float $value;
 
     public function __construct(int $timestamp, float $value)
     {
+        // ASK -> time in constant
         $this->date = Carbon::createFromTimestamp($timestamp / 1000);
         $this->value = $value;
     }
