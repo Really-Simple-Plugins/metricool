1e1f3c7a8205275c53bd42f3082f24c5d3f4cfee
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-10-15 00:56:20 +0200

    feat: implemented DataTable for countries

--- react/src/custom/CountriesTab.tsx
+++ react/src/custom/CountriesTab.tsx
@@ -2,27 +2,52 @@ import FlexContainer from "./FlexContainer.tsx";
 import { Chart } from "react-google-charts";
 import { useQuery } from "@tanstack/react-query";
 import { useGlobalContext } from "../context/GlobalContext.tsx";
-import { Button } from "../components";
+import { Button, DataTable, DataTableColumnHeader, type Column } from "../components";
 import { __ } from "@wordpress/i18n";
+
+type DataTableColumns = { country: string, visitors: unknown, percent: string };
+
+const columns = [
+    {
+        accessorKey: "country",
+        header: ({ column }: { column: Column<DataTableColumns> }) => (<DataTableColumnHeader column={column} title={__("Country", "metricool")}/>),
+    },
+    {
+        accessorKey: "visitors",
+        header: ({ column }: { column: Column<DataTableColumns> }) => (<DataTableColumnHeader column={column} title={__("Visitors", "metricool")}/>),
+    },
+    {
+        accessorKey: "percent",
+        header: ({ column }: { column: Column<DataTableColumns> }) => (<DataTableColumnHeader column={column} title={__("Percent", "metricool")}/>),
+    },
+];
 
 const CountriesTab = () => {
     const { metricool, httpClient } = useGlobalContext();
     const localizeCountryNames = new Intl.DisplayNames(metricool.locale, { type: "region" });
+    const numberFormatter = Intl.NumberFormat(metricool.locale, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 3,
+    });
     const { data: countryData, isLoading, error } = useQuery({
         queryKey: ["analytics", "countries"],
         queryFn: () => httpClient?.setRoute("statistics/countries").get(),
         staleTime: 1000 * 60 * 5, // 5 minutes
         select: (data) => {
+            const totalVisitors = Object.values(data.data).reduce((previous, current) => Number(previous) + Number(current));
             const countriesArray = Object.entries(data.data)
                 .map(([countryCode, visitorAmount]) => [countryCode, localizeCountryNames.of(countryCode), visitorAmount]);
-            return [["value", "Country", __("Visitors", "metricool")], ...countriesArray]
+            return {
+                chartData: [["value", "Country", __("Visitors", "metricool")], ...countriesArray],
+                tableData: Object.entries(data.data).map(([countryCode, visitorAmount]) => ({ country: localizeCountryNames.of(countryCode) ?? "", visitors: visitorAmount, percent: `${numberFormatter.format((Number(visitorAmount) * 100) / Number(totalVisitors))}%` })),
+            };
         }
     });
 
     const geochartOptions = {
         datalessRegionColor: "white",
         backgroundColor: "#A6CEE3",
-        colorAxis: { colors: ["white",  "#E6A735",  "#E6A735",  "#E6A735",  "#E6A735",  "#E6A735",  "#E6A735",  "#E6A735",  "#E6A735" ] },
+        colorAxis: { colors: ["white", "#E6A735", "#E6A735", "#E6A735", "#E6A735", "#E6A735", "#E6A735", "#E6A735", "#E6A735"] },
         defaultColor: "white",
         legend: "none",
     };
@@ -33,16 +58,17 @@ const CountriesTab = () => {
                 <div>LOADING</div>
             )}
             {countryData && (
-                <FlexContainer direction={"column"} className={"rounded-md bg-gray-50"}>
+                <FlexContainer direction={"column"}>
                     <FlexContainer direction={"column"} className={"rounded-md overflow-hidden"}>
                         <Chart
-                            data={countryData}
+                            data={countryData.chartData}
                             chartType="GeoChart"
                             options={geochartOptions}
                             height={"200px"}
                             width={"100%"}
                         />
                     </FlexContainer>
+                    <DataTable columns={columns} data={countryData.tableData} tableSettings={{ pageSize: 5 }}/>
                 </FlexContainer>
             )}
             {error && (
