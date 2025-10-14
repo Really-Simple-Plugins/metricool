926d9f378781c6736b905a58d46728a8223e6dc6
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-10-14 15:22:28 +0200

    refactor: moved Statistic to TimelineStatistic and added phpdoc

--- app/http/metricool/DTO/TimelineStatistic.php
+++ app/http/metricool/DTO/TimelineStatistic.php
@@ -2,23 +2,39 @@
 
 namespace Metricool\Http\Metricool\Dto;
 
-class Statistic // ASK -> name too generic?
+/**
+ * This class represents a TimelineStatistic of one of the results of
+ * the TimelineStatistic Entity.
+ */
+class TimelineStatistic
 {
     public int $timestamp;
     public float $hits;
 
+    /**
+     * Constructor to fill all the properties of the TimelineStatistic
+     * @param int $timestamp
+     * @param float $hits
+     */
     public function __construct(int $timestamp, float $hits)
     {
-        // ASK -> time in constant
         $this->timestamp = $timestamp;
         $this->hits = $hits;
     }
 
+    /**
+     * Gets the hits
+     * @return float
+     */
     public function getHits() : float
     {
         return $this->hits;
     }
 
+    /**
+     * Gets the timestamp
+     * @return int
+     */
     public function getTimestamp() : int
     {
         return $this->timestamp;
