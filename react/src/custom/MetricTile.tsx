8aeb7e773c6598176910cb20c42f0f011eeeb759
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-10-14 12:12:17 +0200

    feat: changed implementation of MetricTile
    - changed direction to trend to be in line with backend
    - Removed label as prop and made it a child
    - Added inactive and disabled states and styling

--- react/src/custom/MetricTile.tsx
+++ react/src/custom/MetricTile.tsx
@@ -2,26 +2,29 @@ import { clsx } from "clsx";
 import { Icon } from "../components";
 
 type MetricTileProps = {
-    variant: "primary" | "primary-dark" | "secondary" | "tertiary" | "light-green",
-    direction?: "up" | "down" | "stable",
+    variant?: string,
+    trend?: "up" | "down" | "stable",
     metric: string | number,
-    metricTitle: string,
+    inactive?: boolean,
+    disabled?: boolean,
 }
 
-const MetricTile = ({ className, variant, direction, metric, metricTitle }: React.ComponentProps<"div"> & MetricTileProps) => {
+const MetricTile = ({ children, className, variant, trend, metric, inactive, disabled, ...props }: React.ComponentProps<"div"> & MetricTileProps) => {
     return (
-        <div className={clsx(className, "flex flex-col px-4 py-2 rounded-sm min-w-[90px]",
+        <div {...props} className={clsx(className, "flex flex-col px-4 py-2 rounded-sm min-w-[80px]",
             variant === "primary" && "bg-primary",
             variant === "primary-dark" && "bg-primary-dark",
             variant === "secondary" && "bg-secondary",
             variant === "tertiary" && "bg-tertiary",
             variant === "light-green" && "bg-light-green",
+            (props.onClick && !disabled) && "hover:cursor-pointer",
+            (inactive || disabled) && "opacity-35 transition-all ease-in-out duration-300",
         )}>
-            <div className={clsx("flex flex-row text-white text-xl", direction ? "justify-between" : "justify-center")}>
+            <div className={clsx("flex flex-row text-white text-xl items-center gap-2", trend ? "justify-between" : "justify-center")}>
                 {metric}
-                {direction && <Icon icon={direction} iconClass={"text-white"}/>}
+                {trend && <Icon icon={trend} iconClass={"text-white"}/>}
             </div>
-            <div className={"flex w-full items-center justify-center text-white text-sm"}>{metricTitle}</div>
+            <div className={"flex w-full items-center justify-center text-white text-sm"}>{children}</div>
         </div>
     );
 };
