20d29fd627c516a42df5628f202e37d2557dc00a
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-10-20 14:17:29 +0200

    style: double quotes and props in curly braces

--- react/src/custom/Header.tsx
+++ react/src/custom/Header.tsx
@@ -1,4 +1,4 @@
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 import { useGlobalContext } from "../context/GlobalContext.tsx";
 import { Button } from "../components";
 import HeaderTab from "./HeaderTab.tsx";
@@ -6,15 +6,15 @@ import HeaderTab from "./HeaderTab.tsx";
 const Header = () => {
     const { metricool } = useGlobalContext();
     return (
-        <div className="bg-white min-w-full mx-auto px-4 flex justify-between items-center">
+        <div className={"bg-white min-w-full mx-auto px-4 flex justify-between items-center"}>
             <div className={"flex gap-8"}>
                 <img src={`${metricool.assets_url}img/mc-logo.svg`} alt={"Metricool logo"}/>
-                <div className="flex gap-4">
-                    <HeaderTab link="/">
-                        {__('Dashboard', 'metricool')}
+                <div className={"flex gap-4"}>
+                    <HeaderTab link={"/"}>
+                        {__("Dashboard", "metricool")}
                     </HeaderTab>
-                    <HeaderTab link="/settings">
-                        {__('Settings', 'metricool')}
+                    <HeaderTab link={"/settings"}>
+                        {__("Settings", "metricool")}
                     </HeaderTab>
                     <HeaderTab link={`https://app.metricool.com/planner/calendar?blogId=${metricool.blogId}&userId=${metricool.userId}`} external={true}>
                         {__("Planner", "metricool")}
