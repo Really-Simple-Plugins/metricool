9afd710a4205363677e819ff72a0c785f53a5227
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-10-20 14:50:17 +0200

    refactor: make DTO abstract and improved phpdoc

--- app/http/metricool/DTOs/DTO.php
+++ app/http/metricool/DTOs/DTO.php
@@ -3,19 +3,26 @@
 namespace Metricool\Http\Metricool\DTOs;
 
 /**
- * DTO class to store array into an object
- * You can dynamically access properties into methods.
- * Defining an accessor: public function getCountryAttribute()
- * $dto->country will call the accessor
+ * DTO class to store arrays into an object
+ * Dynamically access properties through magic method.
+ * You can define an accessor with (for example) getCountryAttribute()
+ * $dto->country will call the getCountryAttribute accessor method
  */
-class DTO
+abstract class DTO
 {
     public function __get($key)
     {
         $method = 'get' . ucfirst($key) . 'Attribute';
+
         if (method_exists($this, $method)) {
             return $this->$method();
         }
-        return '...';
+
+        return null;
     }
+
+    /**
+     * Returns the serialized version of the DTO
+     */
+    abstract public function toArray(): array;
 }
