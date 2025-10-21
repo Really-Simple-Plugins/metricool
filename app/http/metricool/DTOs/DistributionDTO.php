1a211420f7050416199e41d7b0177c045d4ae221
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-10-21 15:24:17 +0200

    refactor: renamed DistributionStatistic to DistributionDTO and made non-abstract

---
+++ app/http/metricool/DTOs/DistributionDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Metricool\Http\Metricool\DTOs;
+
+/**
+ * This class represents a DistributionDTO of one of the results of
+ * the DistributionDTO Entity.
+ * {@see \Metricool\Http\Metricool\}
+ */
+class DistributionDTO extends DTO
+{
+    public string $name;
+    public string $value;
+    public int $amount;
+
+    public function __construct(string $name, string $value, int $amount)
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->amount = $amount;
+    }
+
+    public function calculatePercentageFromTotal($total)
+    {
+        if($total === 0 || $this->amount === 0) {
+            return 0;
+        }
+        return round((float) (($this->amount / $total) * 100), 3, PHP_ROUND_HALF_UP);
+    }
+}
