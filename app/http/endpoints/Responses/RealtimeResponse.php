d4007ad2eefbee2935166b12e0c0ba4494177f30
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-10-23 13:03:22 +0200

    refactor: removed metric method from RealtimeResponse, moved to service

--- app/http/endpoints/Responses/RealtimeResponse.php
+++ app/http/endpoints/Responses/RealtimeResponse.php
@@ -14,22 +14,6 @@ class RealtimeResponse extends Response
     }
 
     /**
-     * Adds a metric to the service. The results will be hydrated and ordered before being added to the service.
-     * @param $results array{timestamp: int, amount: float} Results from a Metricool timeline response
-     */
-    public function addMetric(string $name, string $label, array $results, bool $useInTimeline = true): self
-    {
-        // create an array of TimelineDTO's and sort them
-        $results = $this->hydrateTimelineResults($results)->sortBy('timestamp');
-
-        // add the sorted results to the service
-        $this->service->addMetric($name, $label, $results, $useInTimeline);
-
-        return $this;
-    }
-
-
-    /**
      * Creates the response body
      */
     public function body(): array
