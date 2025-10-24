001322f6842512ce15a3c7322fa97b9850281184
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-10-24 19:06:46 +0200

    fix: Visual Check comment #3: Menu Item active state

--- react/src/custom/HeaderTab.tsx
+++ react/src/custom/HeaderTab.tsx
@@ -7,7 +7,7 @@ const HeaderTab = ({ link, children, external = false }: {
     external?: boolean
 }) => {
     return (
-        <Link to={link} {...(external && { target: "_blank" })} className="text-md text-black items-center flex gap-1 py-[23px] focus:outline-hidden relative ease-in-out duration-100 [&.active]:border-b-2 [&.active]:border-b-primary hover:border-b-2 hover:border-b-primary-light hover:text-black focus:shadow-none">
+        <Link to={link} {...(external && { target: "_blank" })} className="text-md text-black items-center flex gap-1 py-[23px] focus:outline-hidden relative ease-in-out duration-100 border-b-2 border-b-transparent [&.active]:border-b-primary hover:border-b-2 hover:border-b-primary-light hover:text-black focus:shadow-none">
             {children}
             {external && (<Icon icon={"inline-external-link"}/>)}
         </Link>
