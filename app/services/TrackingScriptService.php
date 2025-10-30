713704a32383c3d33a41d643eb3658cd18d9e298
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-10-30 12:10:31 +0100

    refactor: improved phpdoc and return types in TrackingScriptService

--- app/services/TrackingScriptService.php
+++ app/services/TrackingScriptService.php
@@ -4,18 +4,28 @@ namespace Metricool\Services;
 
 class TrackingScriptService
 {
-    public function getTrackingWidgetActive(): bool
-    {
-        return get_option('metricool_tracking_script_active');
-    }
-
-    public function getTrackingHash()
-    {
-        return get_option('metricool_tracking_script_hash');
-    }
-
+    /**
+     * Checks if a hash is set and if the user has enabled the widget
+     */
     public function trackingScriptActive(): bool
     {
-        return !empty($this->getTrackingHash()) && !empty($this->getTrackingWidgetActive());
+        return !empty($this->getTrackingHash()) && $this->getTrackingWidgetActive();
     }
+
+    /**
+     * Returns if the user has enabled the widget in the settings
+     */
+    public function getTrackingWidgetActive(): bool
+    {
+        return get_option('metricool_tracking_script_active') ?? true;
+    }
+
+    /**
+     * Returns the hash from settings
+     */
+    public function getTrackingHash(): string
+    {
+        return get_option('metricool_tracking_script_hash') ?? '';
+    }
+
 }
