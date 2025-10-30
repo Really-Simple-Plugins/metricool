743d22f379c520bb4ddce9be45bddd0eb2882275
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-10-30 12:53:00 +0100

    feat: implemented actual notifications fetching from backend

--- react/src/custom/NotificationsSidebar.tsx
+++ react/src/custom/NotificationsSidebar.tsx
@@ -1,16 +1,68 @@
-import { Block, BlockHeader, BlockHeaderTitle } from "../components";
+import { Block, BlockHeader, BlockHeaderTitle, Alert, FlexContainer } from "../components";
 import { __ } from "@wordpress/i18n";
+import { useQuery } from "@tanstack/react-query";
+import { useGlobalContext } from "../context/GlobalContext.tsx";
+import { useEffect } from "react";
+
+type Notice = {
+    action: {
+        text: string,
+        link: string,
+        target: string,
+    },
+    active: boolean,
+    id: string,
+    premium: boolean,
+    route: string,
+    text: string,
+    title: string,
+    type: "info" | "warning",
+};
 
 const NotificationsSidebar = () => {
+    const { httpClient } = useGlobalContext();
+    const { data: noticeData, isLoading, error } = useQuery({
+        enabled: !!httpClient,
+        queryKey: ["notices"],
+        queryFn: () => httpClient?.setRoute("get_notices").get(),
+        staleTime: 1000 * 60, // 1 minute
+        select: (data): Notice[] => {
+            console.log(data);
+            return data.data;
+        },
+    });
+
+    useEffect(() => {
+        console.log(noticeData, isLoading, error,);
+    }, [noticeData, isLoading, error,]);
+
     return (
-        <Block variant={"transparent"}>
+        <Block variant={"transparent"} className={"px-0"}>
             <BlockHeader className={"!gap-3"}>
                 <BlockHeaderTitle>
                     {__("Notifications", "metricool")}
                 </BlockHeaderTitle>
                 <hr/>
             </BlockHeader>
-            <div className={"text-gray-400 italic"}>{__("You currently have no notifications.", "metricool")}</div>
+            {noticeData ? (
+                noticeData.map((notice) => (
+                    <Alert title={notice.title} variant={notice.type} >
+                        <FlexContainer direction={"column"} className={"!gap-2"}>
+                        <div>{notice.text}</div>
+                            {notice.action && (
+                                <div className={"p-0.5"}>
+                                    <a href={notice.action.link} target={notice.action.target}>
+                                        {notice.action.text}
+                                    </a>
+                                </div>
+                            )}
+                        </FlexContainer>
+                    </Alert>))
+            ) : (
+                <div className={"text-gray-400 italic"}>
+                    {__("You currently have no notifications.", "metricool")}
+                </div>
+            )}
         </Block>
     );
 };
