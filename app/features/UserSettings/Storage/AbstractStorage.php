4bd1226aab91a3260eb7cef8f149948c9e92605b
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-11-11 17:01:37 +0100

    refactor: use camelCase in storage and added exception to casing

--- app/features/UserSettings/Storage/AbstractStorage.php
+++ app/features/UserSettings/Storage/AbstractStorage.php
@@ -7,12 +7,11 @@ use Metricool\Utility\StringUtility;
 abstract class AbstractStorage
 {
     public string $name;
-    protected string $casing = 'snake_case';
+    protected string $casing;
 
     public function __construct($name, array $config)
     {
         $this->name = $name;
-        $this->casing = $config['casing'] ?? 'snake_case';
     }
 
     abstract public function get(string $key);
@@ -26,14 +25,16 @@ abstract class AbstractStorage
     protected function convertCase(string $key): string
     {
         switch ($this->casing) {
-            case 'pascal_case':
+            case 'pascalCase':
                 return StringUtility::snakeToPascalCase($key);
-            case 'camel_case':
+            case 'camelCase':
                 return StringUtility::snakeToCamelCase($key);
-            case 'snake_case':
+            case 'snakeCase':
                 return StringUtility::camelToSnakeCase($key);
-            default:
+            case '':
                 return $key;
+            default:
+                throw new \InvalidArgumentException('Unknown casing type: ' . $this->casing . ' for storage: ' . $this->name . '.');
         }
     }
 }
