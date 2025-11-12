debcae53d8051899de64b4add150ac8810736d5c
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-11-12 23:25:10 +0100

    fix: remove debug

--- app/features/UserSettings/Validators/RequiredValidator.php
+++ app/features/UserSettings/Validators/RequiredValidator.php
@@ -12,9 +12,6 @@ class RequiredValidator extends AbstractValidator
      */
     public function validate($value, \WP_REST_Request $request = null): void
     {
-        var_dump($value);
-        var_dump($this->isEmptyValue($value));
-        exit;
         if ($this->isEmptyValue($value)) {
             throw new ValidatorFailedException(__('Please enter a value', 'metricool'), $this);
         }
