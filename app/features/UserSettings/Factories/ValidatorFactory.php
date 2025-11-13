565825d1bc3238b07aab764f1228bf171c075d38
Kelvin Kuiper <kelvin.kuiper@team.blue>
2025-11-13 11:56:37 +0100

    refactor: improved ValidatorFactory codestyle

--- app/features/UserSettings/Factories/ValidatorFactory.php
+++ app/features/UserSettings/Factories/ValidatorFactory.php
@@ -8,30 +8,35 @@ class ValidatorFactory
 {
     private const VALIDATORS_NAMESPACE = '\\Metricool\\Features\\UserSettings\\Validators\\';
 
-    public static function create(string $validator, Field $field)
+    /**
+     * Creates a validator instance from the configuration string
+     * It will look in the Validators namespace
+     */
+    public static function create(string $validatorConfig, Field $field)
     {
         // Extract the name and parameters from the validator string
-        $validator = self::parseValidatorString($validator);
+        $validatorInfo = self::parseValidatorConfig($validatorConfig);
 
-        $validatorClass = self::VALIDATORS_NAMESPACE . ucfirst($validator['name']) . 'Validator';
-
+        $validatorClass = self::VALIDATORS_NAMESPACE . ucfirst($validatorInfo['className']);
         if (!class_exists($validatorClass)) {
-            throw new \InvalidArgumentException('Validator "' . $validator['name'] . '" not found');
+            throw new \InvalidArgumentException('Validator "' . $validatorInfo ['className'] . '" not found');
         }
 
-        return new $validatorClass($field, ...$validator['params']);
+        return new $validatorClass($field, ...$validatorInfo ['params']);
     }
 
     /**
-     * Parse a validator string into an array with a name and parameters
-     * Example string: "requiredIf:sendToAlternativeEmail,true"
+     * Parse a validator string into an array with the name and parameters
+     * Example: "requiredIf:sendToAlternativeEmail,true"
+     * Becomes: ['name' => 'requiredIf', 'params' => ['sendToAlternativeEmail', 'true']]
+     * @see config/user_settings
      */
-    protected static function parseValidatorString(string $validator): array
+    protected static function parseValidatorConfig(string $validator): array
     {
         $parts = explode(':', $validator);
 
         return [
-            'name' => $parts[0],
+            'className' => $parts[0] . 'Validator',
             'params' => (count($parts) > 1) ? explode(',', $parts[1]) : [],
         ];
     }
