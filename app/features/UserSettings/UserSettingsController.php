31b76dc301b0c62f2f49f97f3acf50b0c4b1c6c8
willembraam <willem.braam@team.blue>
2025-11-17 16:18:53 +0100

    refactor: made controller responsible for converting config file to objects

--- app/features/UserSettings/UserSettingsController.php
+++ app/features/UserSettings/UserSettingsController.php
@@ -2,11 +2,15 @@
 
 namespace Metricool\Features\UserSettings;
 
+use Metricool\App;
+use Metricool\Helpers\Collection;
 use Metricool\Interfaces\FeatureInterface;
+use Metricool\Features\UserSettings\Factories\FieldFactory;
+use Metricool\Features\UserSettings\Factories\StorageFactory;
+use Metricool\Features\UserSettings\Exceptions\StorageNotFoundException;
 
 class UserSettingsController implements FeatureInterface
 {
-    private UserSettingsService $service;
     private UserSettingsEndpoint $endpoints;
 
     /**
@@ -14,12 +18,53 @@ class UserSettingsController implements FeatureInterface
      */
     public function __construct()
     {
-        $this->service = new UserSettingsService();
-        $this->endpoints = new UserSettingsEndpoint($this->service);
+        $convertedStorages = $this->convertedUserSettingStorages();
+        $convertedFields = $this->convertedUserSettingFields($convertedStorages);
+
+        $this->endpoints = new UserSettingsEndpoint(
+            new UserSettingsService($convertedStorages, $convertedFields)
+        );
     }
 
     public function register()
     {
         $this->endpoints->register();
+    }
+
+    /**
+     * Initialize classes for storages from user_settings configuration and
+     * return it as a Collection.
+     */
+    private function convertedUserSettingStorages(): Collection
+    {
+        $storages = [];
+        foreach (App::userSettings('storages', []) as $name => $config) {
+            $storages[$name] = StorageFactory::createFromConfig($name, $config);
+        }
+
+        return new Collection($storages);
+    }
+
+    /**
+     * Initialize fields from user_settings "fields" configuration
+     * @throws StorageNotFoundException when a field's storage is not present in the storages configuration
+     */
+    private function convertedUserSettingFields(Collection $convertedStorages): Collection
+    {
+        $fields = [];
+        foreach (App::userSettings('fields', []) as $name => $config) {
+            $field = FieldFactory::createFromConfig($name, $config);
+            $storage = $convertedStorages->where('name', $field->getStorage())->first();
+
+            // Abort when storage not present in config
+            if ($storage == null) {
+                throw new StorageNotFoundException('Storage "' . $field->getStorage() . '" not found for field: ' . $field->getName());
+            }
+
+            // Add the field to the list
+            $fields[$name] = $field;
+        }
+
+        return new Collection($fields);
     }
 }
