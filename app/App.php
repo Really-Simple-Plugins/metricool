d6b59c5e2d3b4831d9294a78428821d5203b0ae3
willembraam <willem.braam@team.blue>
2025-11-17 15:23:19 +0100

    docs: added phpdoc to method userSettings

--- app/App.php
+++ app/App.php
@@ -62,8 +62,10 @@ final class App
         return (empty($key) ? self::$related : self::$related->get($key));
     }
 
-
-    public static function userSettings()
+    /**
+     * Static method to get the user settings' config.
+     */
+    public static function userSettings(): Dot
     {
         if (empty(self::$userSettings)) {
             self::$userSettings = self::dotFromPath(dirname(__FILE__, 2) . '/config/user_settings.php');
