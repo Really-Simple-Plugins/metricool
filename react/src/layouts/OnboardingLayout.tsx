011747a9a8d35723906e5fb13eb0b15dccec77a6
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-11-18 16:01:22 +0100

    feat: added a fully styled Header

--- react/src/layouts/OnboardingLayout.tsx
+++ react/src/layouts/OnboardingLayout.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
 import { __ } from '@wordpress/i18n';
+import { Button, FlexContainer } from "../components";
+import { useGlobalContext } from "../context/GlobalContext.tsx";
 
-export const OnboardingLayout = ({ children }: { children?: React.ReactNode }) => {
+export const OnboardingLayout = () => {
+    const { metricool } = useGlobalContext();
     return (
-        <div>
-            <div className='p-2 flex gap-2'>
-                {__('Onboarding', 'metricool')}
-            </div>
-            <hr/>
-            {children}
-        </div>
+        <FlexContainer direction={"column"} className={"w-full h-full px-20 py-16"}>
+            <FlexContainer direction={"row"} className={"justify-between items-center"}>
+                <FlexContainer direction={"row"} className={"text-base font-bold font-nunito items-center"}>
+                    <img src={`${metricool.assets_url}img/mc-logo.svg`} alt={"Metricool logo"}/>
+                    <img src={`${metricool.assets_url}img/logo.svg`} className={"h-[30px]"} alt={"Metricool logo"}/>
+                    {__("The digital Swiss Army Knife for social media marketers", "metricool")}
+                </FlexContainer>
+                <FlexContainer direction={"row"} className={"text-md font-semibold font-nunito items-center"}>
+                    {__("Already a Metricooler?", "metricool")}
+                    <Button variant={"primary-gradient-ghost"} className={"p-0 after:!bg-white after:!border-none !border-none"} onClick={() => console.log("hi")}>
+                        {__("Sign in here", "metricool")}
+                    </Button>
+                </FlexContainer>
+            </FlexContainer>
+            <div className={"w-full h-[2px] bg-[image:var(--gradient-brand-secondary)]"}></div>
+        </FlexContainer>
     );
 };
