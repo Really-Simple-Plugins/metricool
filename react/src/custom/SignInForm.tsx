75adad159215fdec357ebf35f51bfe03f47411d2
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-12-11 00:39:51 +0100

    feat: implemented FieldWrapper, adjusted styling and made ids on inputs unique

--- react/src/custom/SignInForm.tsx
+++ react/src/custom/SignInForm.tsx
@@ -1,15 +1,14 @@
-import { Button, FlexContainer, Icon, Input, Label } from "../components";
+import { Button, FieldWrapper, FlexContainer, Input } from "../components";
 import { __ } from "@wordpress/i18n";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { clsx } from "clsx";
 
 const formSchema = z.object({
-    email: z.email({
+    signInEmail: z.email({
         error: () => __("Please enter a valid email address", "metricool"),
     }),
-    password: z.string().min(8, {
+    signInPassword: z.string().min(8, {
         error: () => __("Password must be at least 8 characters", "metricool"),
     }),
 }).required();
@@ -20,55 +19,74 @@ type SignInFormProps = {
 const SignInForm = ({ onSubmit }: SignInFormProps) => {
     const {
         handleSubmit,
-        formState: { errors: formValidationErrors, dirtyFields },
+        formState: { dirtyFields },
         control,
     } = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            email: "",
-            password: "",
+            signInEmail: "",
+            signInPassword: "",
         },
     });
 
     return (
         <FlexContainer direction={"row"}>
-            <form onSubmit={handleSubmit((values) => onSubmit(values))} className={"flex flex-col items-center justify-center gap-2"}>
-                <FlexContainer direction={"column"} className={"!gap-2"}>
-                    <FlexContainer direction={"column"} className={"!gap-2"}>
-                        <Label htmlFor={"email"} className={"text-black"}>{__("Email", "metricool")}</Label>
-                        <Controller
-                            control={control}
-                            render={({ field }) =>
-                                <Input {...field} id={"email"} placeholder={__("Enter your email", "metricool")} className={"bg-white font-semibold text-black min-w-76 max-w-80"}/>}
-                            name={"email"}
-                        />
-                        <span className={clsx("text-rsp-error-dark text-sm h-0 opacity-0 transition-all ease-in-out duration-600", formValidationErrors.password?.message && "h-3 opacity-100")}>
-                            <Icon icon={"error"} iconClass={"text-rsp-error-dark"} inverse={true}></Icon>
-                            {" "}
-                            {formValidationErrors.email?.message}
-                        </span>
-                    </FlexContainer>
-                    <FlexContainer direction={"column"} className={"!gap-2"}>
-                        <Label htmlFor={"password"} className={"text-black"}>{__("Password", "metricool")}</Label>
-                        <Controller
-                            control={control}
-                            render={({ field }) =>
-                                <Input type={"password"} {...field} id={"password"} placeholder={__("Write your password here", "metricool")} className={"bg-white font-semibold text-black min-w-76 max-w-80"}/>}
-                            name={"password"}
-                        />
-                        <span className={clsx("text-rsp-error-dark text-sm h-0 opacity-0 transition-all ease-in-out duration-600", formValidationErrors.password?.message && "h-3 opacity-100")}>
-                            <Icon icon={"error"} iconClass={"text-rsp-error-dark"} inverse={true}></Icon>
-                            {" "}
-                            {formValidationErrors.password?.message}
-                        </span>
-                    </FlexContainer>
+            <form onSubmit={handleSubmit((values) => onSubmit(values))} className={"flex flex-col items-center justify-center gap-6"}>
+                <FlexContainer direction={"column"}>
+                    <Controller
+                        control={control}
+                        name={"signInEmail"}
+                        render={({ field, fieldState }) => (
+                            <FieldWrapper
+                                required
+                                label={__("Email", "metricool")}
+                                htmlFor={"sign-in-email"}
+                                fieldState={{
+                                    invalid: fieldState.invalid,
+                                    error: { message: fieldState.error?.message }
+                                }}
+                            >
+                                <Input
+                                    {...field}
+                                    aria-invalid={fieldState.invalid}
+                                    id={"sign-in-email"}
+                                    placeholder={__("Enter your email", "metricool")}
+                                    className={"min-w-76 max-w-80"}
+                                />
+                            </FieldWrapper>
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name={"signInPassword"}
+                        render={({ field, fieldState }) => (
+                            <FieldWrapper
+                                required
+                                label={__("Password", "metricool")}
+                                htmlFor={"sign-in-password"}
+                                fieldState={{
+                                    invalid: fieldState.invalid,
+                                    error: { message: fieldState.error?.message }
+                                }}
+                            >
+                                <Input
+                                    {...field}
+                                    aria-invalid={fieldState.invalid}
+                                    id={"sign-in-password"}
+                                    placeholder={__("Write your password here", "metricool")}
+                                    className={"min-w-76 max-w-80"}
+                                    type={"password"}
+                                />
+                            </FieldWrapper>
+                        )}
+                    />
                 </FlexContainer>
                 <Button
                     variant={"black"}
                     icon={"arrow-right"}
                     iconPosition={"right"}
                     type={"submit"}
-                    disabled={!(dirtyFields.email && dirtyFields.password)}
+                    disabled={!(dirtyFields.signInEmail && dirtyFields.signInPassword)}
                 >
                     {__("Sign in", "metricool")}
                 </Button>
