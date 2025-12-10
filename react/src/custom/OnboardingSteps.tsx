a9882e88ac8b09c4316e25fcc926976eaba9dd0f
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-12-11 00:37:13 +0100

    feat: implemented loading ellipses animation

--- react/src/custom/OnboardingSteps.tsx
+++ react/src/custom/OnboardingSteps.tsx
@@ -83,8 +83,8 @@ const LoadingStep = () => {
         <FlexContainer direction={"column"} className={"justify-center items-center"}>
             <DialogHeader className={"!gap-8 justify-center items-center"}>
                 <img src={`${metricool.assets_url}img/metricool_welcome.png`} className={"min-h-[150px] max-h-[150px] w-auto"} alt={"Metricool welcome"}/>
-                <DialogTitle className={"font-bold font-nunito m-0 text-2xl"}>
-                    {__("Creating Awesomeness...", "metricool")}
+                <DialogTitle className={"font-bold font-nunito m-0 text-2xl loading-ellipses"}>
+                    {__("Creating Awesomeness", "metricool")}
                 </DialogTitle>
             </DialogHeader>
             <div className={"text-base text-center"}>
