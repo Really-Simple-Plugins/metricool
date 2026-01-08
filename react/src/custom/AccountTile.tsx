b1cc5f854c613c8a00b18ca8bb07d7046473f494
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-01-08 15:48:49 +0100

    feat: updated the styling of AccountTiles

--- react/src/custom/AccountTile.tsx
+++ react/src/custom/AccountTile.tsx
@@ -1,4 +1,4 @@
-import { FlexContainer, Icon, type IconProps } from "../components";
+import { Button, FlexContainer, Icon, type IconProps } from "../components";
 import { clsx } from "clsx";
 import { __, sprintf } from "@wordpress/i18n";
 
@@ -24,32 +24,56 @@ const AccountTile = ({
     return (
         <FlexContainer
             direction={"row"}
-            onClick={() => {
-                window.open(link, "_blank");
-                window.focus();
-            }}
+            {...(!userName && {
+                onClick: () => {
+                    window.open(link, "_blank");
+                    window.focus();
+                }
+            })}
             className={clsx(
-                "flex rounded-sm border-1 w-full min-h-[48px] px-2 items-center gap-2 cursor-pointer",
-                userName ? "border-neutral-200" : unconnectedClasses,
+                "flex rounded-sm border-1 w-full min-h-[48px] px-2 items-center gap-2",
+                userName ? "border-neutral-200" : `${unconnectedClasses} cursor-pointer transition-all duration-300 ease-in-out`,
             )}
         >
             <FlexContainer direction={"row"} className={"min-w-[25px] items-center justify-center"}>
-                <Icon icon={icon} className={clsx("size-6", userName ? connectedClasses : "text-white")}/>
+                <Icon
+                    data-content
+                    icon={icon}
+                    className={clsx(
+                        "size-6 transition-all duration-300 ease-in-out",
+                        userName ? connectedClasses : "text-white"
+                    )}
+                />
             </FlexContainer>
-            <FlexContainer direction={"row"} className={"justify-between items-center grow"}>
-                <FlexContainer direction={"column"} className={clsx("text-sm justify-center")}>
+            <FlexContainer  direction={"row"} className={"justify-between items-center grow"}>
+                <FlexContainer direction={"column"} className={clsx("text-sm justify-center !gap-0")}>
                     {userName ? (
                         <>
                             <span className={"text-gray-500"}>{label}</span>
                             <div className={"font-semibold"}>{userName}</div>
                         </>
                     ) : (
-                        <span className={"text-white"}>{sprintf(__("Connect a %s Account"), label)}</span>
+                        <span data-content className={"text-white transition-all duration-300 ease-in-out"}>{sprintf(__("Connect a %s Account"), label)}</span>
                     )}
                 </FlexContainer>
-                {upsell && (
-                    <Icon icon={"upsell"} className={"size-2.5 p-0.5 bg-upsell rounded-full text-black"}/>
-                )}
+                <FlexContainer direction={"row"}>
+                    {userName && (
+                        <Button
+                            size={"icon"}
+                            variant={"icon"}
+                            icon={"settings"}
+                            iconClass={userName ? "" : "text-white"}
+                            iconPosition={"left"}
+                            onClick={() => {
+                                window.open(link, "_blank");
+                                window.focus();
+                            }}
+                        />
+                    )}
+                    {upsell && (
+                        <Icon icon={"upsell"} className={"size-2.5 p-0.5 bg-upsell rounded-full text-black"}/>
+                    )}
+                </FlexContainer>
             </FlexContainer>
         </FlexContainer>
     );
