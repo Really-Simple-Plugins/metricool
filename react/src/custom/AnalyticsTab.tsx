4c6e18260af7d07aa0fd02a5e70f273ca2ba77e3
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-01-09 18:53:37 +0100

    feat: added dynamic interval for currentmonth option

--- react/src/custom/AnalyticsTab.tsx
+++ react/src/custom/AnalyticsTab.tsx
@@ -149,7 +149,11 @@ const AnalyticsTab = () => {
                 data: { totals: Record<string, MetricData>, timelineData: TimelineData },
             } = queryClient.getQueryData(["analytics"]) ?? { data: { totals: {}, timelineData: [] } };
             queryClient.setQueryData(["analytics"], { ...currentChartData, data: data });
-            setXAxisInterval(periodFilter.interval);
+            if (periodFilter.option === "currentmonth") {
+                setXAxisInterval(data.timelineData.length < 14 ? 0 : 4);
+            } else {
+                setXAxisInterval(periodFilter.interval);
+            }
         }
     });
 
