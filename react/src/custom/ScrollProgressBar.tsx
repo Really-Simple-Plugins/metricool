a2f8be36ebb511cdc231a6648aba642bc35ee36c
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-01-20 15:49:28 +0100

    style: new lines, squigly braces

--- react/src/custom/ScrollProgressBar.tsx
+++ react/src/custom/ScrollProgressBar.tsx
@@ -1,9 +1,11 @@
 const ScrollProgressBar = ({ scrollProgress }: { scrollProgress: number }) => {
 
     return (
-        <div className="h-1 w-full bg-neutral-200">
-            <div className="h-full bg-blue-500"
-                 style={{ width: `${Math.max(scrollProgress, 5)}%` }}>
+        <div className={"h-1 w-full bg-neutral-200"}>
+            <div
+                className={"h-full bg-blue-500"}
+                style={{ width: `${Math.max(scrollProgress, 5)}%` }}
+            >
             </div>
         </div>
     );
