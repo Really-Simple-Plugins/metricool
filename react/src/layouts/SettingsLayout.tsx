632342a4bc00c043d5752b9b43b7de3790311a66
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-01-22 12:54:28 +0100

    style: spaces, imports

--- react/src/layouts/SettingsLayout.tsx
+++ react/src/layouts/SettingsLayout.tsx
@@ -1,17 +1,16 @@
-import { FlexContainer } from "../components";
+import { FlexContainer, ToastContainer } from "../components";
 import Header from "../custom/Header.tsx";
 import SettingsMenu from "../custom/SettingsMenu.tsx";
 import NotificationsSidebar from "../custom/NotificationsSidebar.tsx";
-import { ToastContainer } from "../components";
 
 export const SettingsLayout = ({ children }: React.ComponentProps<"div">) => {
     return (
         <FlexContainer direction={"column"} className={"h-full w-full min-[125rem]:items-center"}>
-            <Header />
+            <Header/>
             <FlexContainer direction={"column"} className={"px-4 w-full items-start md:flex-row max-w-[125rem]"}>
-                <SettingsMenu />
+                <SettingsMenu/>
                 {children}
-                <NotificationsSidebar />
+                <NotificationsSidebar/>
             </FlexContainer>
             <ToastContainer/>
         </FlexContainer>
