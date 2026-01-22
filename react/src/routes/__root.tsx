b6bc80408c602fecc4076c77ea286d0507217541
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-01-22 13:39:06 +0100

    docs: added JSDocs to __root.tsx

--- react/src/routes/__root.tsx
+++ react/src/routes/__root.tsx
@@ -1,6 +1,16 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { GlobalContextProvider } from "../context/GlobalContext.tsx";
 
+/**
+ * Tanstack Router's entry into the entire app, from which it generates
+ * `routeTree.gen.ts`.
+ *
+ * Used to initiate our custom context provider(s) so they are available to the
+ * entire app. The app is rendered as an `<Outlet/>` here, making
+ * {@link Index} a child of this root component. This hierarchy is crucial
+ * as {@link Index} needs to immediately access the context data.
+ *
+ */
 export const Route = createRootRoute({
     component: () => {
         return (
