b423133aa481e94e5b446355f2702a14b73b0a7c
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-01-22 08:37:13 +0100

    feat: moved heading out of OnboardingForm component so it cleanly only returns the form as the name suggests

--- react/src/custom/Onboarding/OnboardingForm.tsx
+++ react/src/custom/Onboarding/OnboardingForm.tsx
@@ -32,135 +32,131 @@ const OnboardingForm = ({ onSubmit }: OnboardingFormProps) => {
     });
 
     return (
-        <FlexContainer direction={"column"} className={"min-w-[45%] max-w-[45%]"}>
-            <h1 className={"font-bold font-nunito text-[1.75rem] leading-8"}>{__("Join more than 2 million professionals, agencies and brands that use Metricool as their one-stop shop for social media and online ad management.", "metricool")}</h1>
-            <form onSubmit={handleSubmit((values) => onSubmit(values))} className={"flex flex-col gap-4"}>
-                <FlexContainer direction={"column"}>
-                    <Controller
-                        control={control}
-                        name={"credentials.email"}
-                        render={({ field, fieldState }) => (
-                            <FieldWrapper
-                                required
-                                label={__("Email", "metricool")}
-                                htmlFor={"sign-up-email"}
-                                fieldState={{
-                                    invalid: fieldState.invalid,
-                                    error: { message: fieldState.error?.message }
-                                }}
-                            >
-                                <Input
-                                    {...field}
-                                    aria-required
-                                    aria-invalid={fieldState.invalid}
-                                    id={"sign-up-email"}
-                                    placeholder={__("Enter your email", "metricool")}
-                                    className={"max-w-5/6"}
+        <form onSubmit={handleSubmit((values) => onSubmit(values))} className={"flex flex-col gap-4"}>
+            <FlexContainer direction={"column"}>
+                <Controller
+                    control={control}
+                    name={"credentials.email"}
+                    render={({ field, fieldState }) => (
+                        <FieldWrapper
+                            required
+                            label={__("Email", "metricool")}
+                            htmlFor={"sign-up-email"}
+                            fieldState={{
+                                invalid: fieldState.invalid,
+                                error: { message: fieldState.error?.message }
+                            }}
+                        >
+                            <Input
+                                {...field}
+                                aria-required
+                                aria-invalid={fieldState.invalid}
+                                id={"sign-up-email"}
+                                placeholder={__("Enter your email", "metricool")}
+                                className={"max-w-5/6"}
+                            />
+                        </FieldWrapper>
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name={"credentials.password"}
+                    render={({ field, fieldState }) => (
+                        <FieldWrapper
+                            required
+                            label={__("Password", "metricool")}
+                            htmlFor={"sign-up-password"}
+                            fieldState={{
+                                invalid: fieldState.invalid,
+                                error: { message: fieldState.error?.message }
+                            }}
+                        >
+                            <Input
+                                {...field}
+                                aria-required
+                                aria-invalid={fieldState.invalid}
+                                id={"sign-up-password"}
+                                placeholder={__("Write your password here", "metricool")}
+                                className={"max-w-5/6"}
+                                type={"password"}
+                            />
+                        </FieldWrapper>
+                    )}
+                />
+            </FlexContainer>
+            <Button
+                variant={"primary-gradient"}
+                icon={"arrow-right"}
+                iconPosition={"right"}
+                type={"submit"}
+                disabled={!(dirtyFields.credentials?.email && dirtyFields.credentials?.password)}
+            >
+                {__("Create your free account", "metricool")}
+            </Button>
+            <FlexContainer direction={"column"}>
+                <Controller
+                    control={control}
+                    name={"terms"}
+                    render={({ field, fieldState }) => (
+                        <FieldWrapper
+                            flexDirection={"row-reverse"}
+                            className={"!gap-3 justify-end"}
+                            label={(
+                                <span
+                                    className={"required-asterisk"}
+                                    dangerouslySetInnerHTML={{
+                                        __html:
+                                            DOMPurify.sanitize(
+                                                sprintf(
+                                                    /*! translators: the two variables are opening and closing anchor tags */
+                                                    __("I have read and accept the %sLegal Terms%s by Metricool.", "metricool"),
+                                                    `<a href=${metricool.trusted_urls.legal_terms} target="_blank">`,
+                                                    `</a>`),
+                                                { ADD_ATTR: ["target"] }
+                                            )
+                                    }}
                                 />
-                            </FieldWrapper>
-                        )}
-                    />
-                    <Controller
-                        control={control}
-                        name={"credentials.password"}
-                        render={({ field, fieldState }) => (
-                            <FieldWrapper
-                                required
-                                label={__("Password", "metricool")}
-                                htmlFor={"sign-up-password"}
-                                fieldState={{
-                                    invalid: fieldState.invalid,
-                                    error: { message: fieldState.error?.message }
-                                }}
-                            >
-                                <Input
-                                    {...field}
-                                    aria-required
-                                    aria-invalid={fieldState.invalid}
-                                    id={"sign-up-password"}
-                                    placeholder={__("Write your password here", "metricool")}
-                                    className={"max-w-5/6"}
-                                    type={"password"}
-                                />
-                            </FieldWrapper>
-                        )}
-
-                    />
-                </FlexContainer>
-                <Button
-                    variant={"primary-gradient"}
-                    icon={"arrow-right"}
-                    iconPosition={"right"}
-                    type={"submit"}
-                    disabled={!(dirtyFields.credentials?.email && dirtyFields.credentials?.password)}
-                >
-                    {__("Create your free account", "metricool")}
-                </Button>
-                <FlexContainer direction={"column"}>
-                    <Controller
-                        control={control}
-                        name={"terms"}
-                        render={({ field, fieldState }) => (
-                            <FieldWrapper
-                                flexDirection={"row-reverse"}
-                                className={"!gap-3 justify-end"}
-                                label={(
-                                    <span
-                                        className={"required-asterisk"}
-                                        dangerouslySetInnerHTML={{
-                                            __html:
-                                                DOMPurify.sanitize(
-                                                    sprintf(
-                                                        /*! translators: the two variables are opening and closing anchor tags */
-                                                        __("I have read and accept the %sLegal Terms%s by Metricool.", "metricool"),
-                                                        `<a href=${metricool.trusted_urls.legal_terms} target="_blank">`,
-                                                        `</a>`),
-                                                    { ADD_ATTR: ["target"] }
-                                                )
-                                        }}
-                                    />
-                                )}
-                                htmlFor={"terms"}
-                                fieldState={{
-                                    invalid: fieldState.invalid,
-                                    error: { message: fieldState.error?.message }
-                                }}
-                            >
-                                <Switch
-                                    aria-required
-                                    id={"terms"}
-                                    aria-invalid={fieldState.invalid}
-                                    checked={field.value}
-                                    onCheckedChange={field.onChange}
-                                />
-                            </FieldWrapper>
-                        )}
-                    />
-                    <Controller
-                        control={control}
-                        name={"marketing"}
-                        render={({ field, fieldState }) => (
-                            <FieldWrapper
-                                flexDirection={"row-reverse"}
-                                className={"!gap-3 justify-end"}
-                                label={__("I wish to receive communications about news and/or promotions from Metricool Software.", "metricool")}
-                                htmlFor={"marketing"}
-                                fieldState={{
-                                    invalid: fieldState.invalid,
-                                    error: { message: fieldState.error?.message }
-                                }}
-                            >
-                                <Switch
-                                    id={"marketing"}
-                                    checked={field.value}
-                                    onCheckedChange={field.onChange}
-                                />
-                            </FieldWrapper>
-                        )}
-                    />
-                </FlexContainer>
-            </form>
-        </FlexContainer>
+                            )}
+                            htmlFor={"terms"}
+                            fieldState={{
+                                invalid: fieldState.invalid,
+                                error: { message: fieldState.error?.message }
+                            }}
+                        >
+                            <Switch
+                                aria-required
+                                id={"terms"}
+                                aria-invalid={fieldState.invalid}
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                            />
+                        </FieldWrapper>
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name={"marketing"}
+                    render={({ field, fieldState }) => (
+                        <FieldWrapper
+                            flexDirection={"row-reverse"}
+                            className={"!gap-3 justify-end"}
+                            label={__("I wish to receive communications about news and/or promotions from Metricool Software.", "metricool")}
+                            htmlFor={"marketing"}
+                            fieldState={{
+                                invalid: fieldState.invalid,
+                                error: { message: fieldState.error?.message }
+                            }}
+                        >
+                            <Switch
+                                id={"marketing"}
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                            />
+                        </FieldWrapper>
+                    )}
+                />
+            </FlexContainer>
+        </form>
     );
 };
 
--- react/src/layouts/OnboardingLayout.tsx
+++ react/src/layouts/OnboardingLayout.tsx
@@ -65,7 +65,10 @@ export const OnboardingLayout = () => {
             </OnboardingHeader>
             <div className={"w-full h-[2px] bg-[image:var(--gradient-brand-secondary)]"}></div>
             <FlexContainer direction={"row"} className={"w-full !gap-0"}>
-                <OnboardingForm onSubmit={(values) => onSubmit(values)}/>
+                <FlexContainer direction={"column"} className={"min-w-[45%] max-w-[45%]"}>
+                    <h1 className={"font-bold font-nunito text-[1.75rem] leading-8"}>{__("Join more than 2 million professionals, agencies and brands that use Metricool as their one-stop shop for social media and online ad management.", "metricool")}</h1>
+                    <OnboardingForm onSubmit={(values) => onSubmit(values)}/>
+                </FlexContainer>
                 <img src={`${metricool.assets_url}img/mc-onboarding-image.webp`} className={"max-w-[55%] h-fit"} alt={__("Laptop and phone displaying the Metricool app", "metricool")}/>
             </FlexContainer>
             <Dialog
