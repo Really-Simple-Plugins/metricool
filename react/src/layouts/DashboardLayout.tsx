0c9ca43b18b20cdbd09bfaa4d9d693854cb31861
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-02-05 13:55:40 +0100

    feat: removed Header and container as these are now in AuthorizedLayout

--- react/src/layouts/DashboardLayout.tsx
+++ react/src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Header, Progress, WebsiteAnalytics, ConnectedAccounts, OtherPlugins,  } from "@/components/custom";
+import { ConnectedAccounts, OtherPlugins, Progress, WebsiteAnalytics, } from "@/components/custom";
 import { Button, Dialog, DialogHeader, DialogTitle, FlexContainer } from "@/components/shared";
 import { useGlobalContext } from "@/context/GlobalContext.tsx";
 import { __ } from "@wordpress/i18n";
@@ -15,8 +15,7 @@ import { __ } from "@wordpress/i18n";
 export const DashboardLayout = () => {
     const { metricool, dispatch } = useGlobalContext();
     return (
-        <FlexContainer direction={"column"} className={"h-full w-full min-[125rem]:items-center"}>
-            <Header/>
+        <>
             <FlexContainer direction={"column"} className={"px-4 w-full max-w-[125rem]"}>
                 <FlexContainer direction={"column"} className={"w-full h-full justify-around xl:flex-row"}>
                     <Progress/>
@@ -55,6 +54,6 @@ export const DashboardLayout = () => {
                     </Button>
                 </FlexContainer>
             </Dialog>
-        </FlexContainer>
+        </>
     );
 };
