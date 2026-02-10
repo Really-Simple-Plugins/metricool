e0b3063e269d3e6ee10b9f918ece554d0a220080
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-02-10 14:52:43 +0100

    feat: added full password validation

--- react/src/components/custom/onboarding/OnboardingSchema.ts
+++ react/src/components/custom/onboarding/OnboardingSchema.ts
@@ -7,9 +7,26 @@ const OnboardingSchema = z.object({
         email: z.email({
             error: () => __("Please enter a valid email address", "metricool"),
         }),
-        password: z.string().min(8, {
-            error: () => __("Password must be at least 8 characters", "metricool"),
-        }),
+        password: z.string()
+            .min(8, {
+                error: () => __("Password must be at least 8 characters", "metricool"),
+            })
+            .max(20, {
+                error: () => __("Password cannot be longer than 20 characters", "metricool"),
+            })
+            .refine((password) => /[A-Z]/.test(password), {
+                error: () => __("Password must contain at least one upper case letter", "metricool"),
+            })
+            .refine((password) => /[a-z]/.test(password),{
+                error: () => __("Password must contain at least one lower case letter", "metricool"),
+            })
+            .refine((password) => /[0-9]/.test(password), {
+                error: () => __("Password must contain at least one number", "metricool"),
+            })
+            .refine((password) => /[!?@#$%^&*]/.test(password), {
+                error: () => __("Password must contain at least one special character", "metricool"),
+            })
+        ,
     }),
     terms: z.boolean().refine((val) => val === true, {
         error: () => __("Please read and accept the Legal Terms", "metricool"),
