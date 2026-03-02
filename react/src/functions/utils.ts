1d32136e0b9bc15e01a301b1679c408b1eae97bb
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-03-02 14:28:46 +0100

    feat: imported new utils with extra function

--- react/src/functions/utils.ts
+++ react/src/functions/utils.ts
@@ -20,3 +20,14 @@ export const capitalizeFirstCharacter = (string: string) => {
 export const isFirstCharacterAVowel = (string: string) => {
     return (/^[aeiou]$/i).test(string.charAt(0));
 };
+
+export const camelCaseToHyphenated = (string: string) => {
+    let hyphenatedString = "";
+    for (let i = 0; i < string.length; i++) {
+        if (i != 0 && /^[A-Z]*$/.test(string.charAt(i))) {
+            hyphenatedString += "-";
+        }
+        hyphenatedString += string.charAt(i).toLowerCase();
+    }
+    return hyphenatedString.replace(".", "-");
+}
