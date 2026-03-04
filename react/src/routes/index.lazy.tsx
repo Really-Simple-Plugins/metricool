ea3f7a999bfa291af326bf5c705b7e0dcce0486b
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-03-04 13:28:21 +0100

    feat: removed safety check as context now initiates with localized data

--- react/src/routes/index.lazy.tsx
+++ react/src/routes/index.lazy.tsx
@@ -30,15 +30,10 @@ function Index() {
     const { metricool } = useGlobalContext();
 
     DOMPurify.addHook("afterSanitizeAttributes", (node) => {
-        // This first check is to ensure the metricool object has been populated
-        // with the data from the backend properly.
-        // todo: remove or simplify check once the context has been set up better
         const listOfAcceptedLinks = Object.values(metricool.trusted_urls);
-        if (!listOfAcceptedLinks.some((link) => link === "")) {
-            const href = node.getAttribute("href");
-            if (href && !listOfAcceptedLinks.includes(href)) {
-                node.remove();
-            }
+        const href = node.getAttribute("href");
+        if (href && !listOfAcceptedLinks.includes(href)) {
+            node.remove();
         }
     });
 
