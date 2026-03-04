bfc903aebbe40bd3f7e5193fe0609f25ac89ca20
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-03-04 10:06:03 +0100

    feat: imported new Input with styling overwrite of WP load-styles

--- react/src/components/shared/forms/Input.tsx
+++ react/src/components/shared/forms/Input.tsx
@@ -9,7 +9,7 @@ const Input = ({ className, type, children, ...props }: React.ComponentProps<"in
     return (
         <PrimitiveInput
             className={cn(
-                "data-[slot=input]:rounded-xs px-2 shadow-none flex bg-white font-semibold text-black",
+                "data-[slot=input]:rounded-xs px-2 shadow-none flex bg-white text-md font-semibold text-black data-[slot=input]:!min-h-9",
                 "data-[slot=input]:aria-invalid:focus-visible:ring-rsp-error-dark/20 data-[slot=input]:focus-visible:aria-invalid:ring-3 data-[slot=input]:aria-invalid:border-rsp-error-dark",
                 className,
             )}
