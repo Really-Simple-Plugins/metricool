3fa297cefe1c38bc14677ee9cb9692717cabbbdf
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-03-19 12:45:21 +0100

    feat: set marketing to true by default

--- react/src/components/custom/onboarding/OnboardingForm.tsx
+++ react/src/components/custom/onboarding/OnboardingForm.tsx
@@ -35,7 +35,7 @@ const OnboardingForm = ({ onSubmit }: OnboardingFormProps) => {
                 password: "",
             },
             terms: false,
-            marketing: false,
+            marketing: true,
         },
     });
 
