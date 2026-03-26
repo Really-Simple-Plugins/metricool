e921e12633792e0540e44f71a3f962176f86cf5b
Kelvin Kuiper <kelvin.kuiper@team.blue>
2026-03-26 17:27:16 +0100

    fix: add global error message for create acount errors

--- app/Features/Onboarding/Services/CreateAccountService.php
+++ app/Features/Onboarding/Services/CreateAccountService.php
@@ -37,6 +37,8 @@ class CreateAccountService
      */
     public function createAccount(string $captcha, string $email, string $password, bool $newsletters): bool
     {
+        $globalError = __('Something went wrong. Please try again or leave a support message on WordPress.org', 'metricool');
+
         // First, check if the password is valid
         if (!$this->isValidPassword($password)) {
             throw new CreateAccountException(__('Password is not valid.', 'metricool'), 'Password is not valid', 422);
@@ -51,24 +53,24 @@ class CreateAccountService
                 'RSPAL-RecaptchaV3Token' => $captcha
             ]);
         } catch (GuzzleException $e) {
-            throw new CreateAccountException(__('Something went wrong.', 'metricool'), $e->getMessage(), 500);
+            throw new CreateAccountException($globalError, $e->getMessage(), 500);
         }
 
         // A 400 response means e-mail exists or the password is invalid
         if ($signupResponse->getStatusCode() == 400) {
-            throw new CreateAccountException(__('Something went wrong.', 'metricool'), 'Email or password error', 422);
+            throw new CreateAccountException($globalError, 'Email or password error', 422);
         }
 
         // Check if the response contains the required fields
         if (empty($signupResponse->data->accessToken) || empty($signupResponse->data->refreshToken)) {
-            throw new CreateAccountException(__('Something went wrong.', 'metricool'), 'Signup response is missing required fields', 500);
+            throw new CreateAccountException($globalError, 'Signup response is missing required fields', 500);
         }
 
         // Parse the user ID from the access token
         $userId = $this->oauth->parseUserIdFromAccessToken($signupResponse->data->accessToken);
 
         if (empty($userId)) {
-            throw new CreateAccountException(__('Something went wrong.', 'metricool'), 'Failed to parse user ID from access token', 500);
+            throw new CreateAccountException($globalError, 'Failed to parse user ID from access token', 500);
         }
 
         // Authenticate the Metricool API Client
@@ -85,9 +87,9 @@ class CreateAccountService
         } catch (GuzzleException $e) {
             // todo: When this happens, the user account is created but the password is not set, so the user can't log in. We should handle this case properly,
             // maybe by deleting the created account or allowing the user to set the password later?
-            $this->api->unAuthenticate();
+            $this->api->logout();
 
-            throw new CreateAccountException(__('Something went wrong', 'metricool'), $e->getMessage(), 500);
+            throw new CreateAccountException($globalError, $e->getMessage(), 500);
         }
 
         // Attempt to automatically set the blog information, complete the onboarding process on success
