9d0993505560752a43a924d9b1a742efdf290e9e
kelvin.kuiper <kelvin.kuiper@team.blue>
2026-05-15 12:10:20 +0000

    fix: load textdomain on init hook instead of plugins_loaded
    Move load_plugin_textdomain from plugins_loaded to init hook as
    required by WordPress 6.7+ to avoid the too-early loading warning.
    
    Written by Devin

--- bootstrap/Plugin.php
+++ bootstrap/Plugin.php
@@ -46,7 +46,7 @@ class Plugin
         register_deactivation_hook($pluginBaseFile, [$this, 'deactivation']);
         register_uninstall_hook($pluginBaseFile, 'Metricool\Bootstrap\Plugin::uninstall');
 
-        add_action('plugins_loaded', [$this, 'loadPluginTextDomain']);
+        add_action('init', [$this, 'loadPluginTextDomain']);
         add_action('plugins_loaded', [$this, 'registerProviders']); // Provide functionality to the plugin
         add_action('metricool_providers_loaded', [$this->featureManager, 'registerFeatures']); // Makes sure features exist when Controllers need them
         add_action('metricool_features_loaded', [$this, 'registerControllers']); // Control the functionality of the plugin
