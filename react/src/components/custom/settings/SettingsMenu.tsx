4365e38eed28744ed5f0a632d6702142b3fca3c1
Kelvin Kuiper <kelvin.kuiper@team.blue>
2026-05-18 15:36:20 +0200

    fix: premium checks in settings navigation

--- react/src/components/custom/settings/SettingsMenu.tsx
+++ react/src/components/custom/settings/SettingsMenu.tsx
@@ -2,9 +2,11 @@ import { Link } from "@tanstack/react-router";
 import { Block, BlockHeader, FlexContainer, ListItem } from "@/components/shared";
 import { __ } from "@wordpress/i18n";
 import { useGlobalContext } from "@/context/GlobalContext.tsx";
+import {cn} from "@/support/functions/utils.ts";
 
 const SettingsMenu = () => {
-    const { metricoolDynamicUrl } = useGlobalContext();
+    const { metricoolDynamicUrl, metricool } = useGlobalContext();
+
     return (
         <Block className={"md:sticky md:top-[3rem]"}>
             <BlockHeader title={__("Settings", "metricool")}/>
@@ -18,10 +20,18 @@ const SettingsMenu = () => {
                 <ListItem className={"text-md text-black cursor-pointer hover:underline"} icon={"inline-external-link"} iconPosition={"right"} link={metricoolDynamicUrl.withPath("affiliation/general")}>
                     {__("Affiliation Program", "metricool")}
                 </ListItem>
-                <ListItem className={"text-md text-upsell font-semibold cursor-pointer hover:underline"} icon={"upsell"} iconClass={"rounded-full bg-upsell size-2.5 p-0.5 text-black"} iconPosition={"right"} link={metricoolDynamicUrl.withPath("user-management/users")}>
+                <ListItem
+                    className={cn("text-md cursor-pointer hover:underline", !metricool.account.is_premium ? "text-upsell font-semibold" : "text-black")}
+                    link={metricoolDynamicUrl.withPath("user-management/users")}
+                    {...(!metricool.account.is_premium && { icon: "upsell", iconClass: "rounded-full bg-upsell size-2.5 p-0.5 text-black", iconPosition: "right" })}
+                >
                     {__("User Management", "metricool")}
                 </ListItem>
-                <ListItem className={"text-md text-upsell font-semibold cursor-pointer hover:underline"} icon={"upsell"} iconClass={"rounded-full bg-upsell size-2.5 p-0.5 text-black"} iconPosition={"right"} link={metricoolDynamicUrl.withPath("my-tasks/open")}>
+                <ListItem
+                    className={cn("text-md cursor-pointer hover:underline", !metricool.account.is_premium ? "text-upsell font-semibold" : "text-black")}
+                    link={metricoolDynamicUrl.withPath("my-tasks/open")}
+                    {...(!metricool.account.is_premium && { icon: "upsell", iconClass: "rounded-full bg-upsell size-2.5 p-0.5 text-black", iconPosition: "right" })}
+                >
                     {__("My Tasks", "metricool")}
                 </ListItem>
             </FlexContainer>
