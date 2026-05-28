2080e230366c62522d72c22c8c8b080f075e45d2
Kelvin Kuiper <kelvin.kuiper@team.blue>
2026-05-28 15:11:24 +0200

    fix: unauthenticated loading of features

--- app/Features/UserSettings/UserSettingsLoader.php
+++ app/Features/UserSettings/UserSettingsLoader.php
@@ -5,15 +5,26 @@ declare(strict_types=1);
 namespace Metricool\Features\UserSettings;
 
 use Metricool\Features\AbstractLoader;
+use Metricool\Services\DashboardService;
+use Metricool\Support\Helpers\Storages\EnvironmentConfig;
+use Metricool\Support\Helpers\Storages\RequestStorage;
 
 class UserSettingsLoader extends AbstractLoader
 {
+    private DashboardService $dashboard;
+
+    public function __construct(EnvironmentConfig $env, RequestStorage $request, DashboardService $dashboard)
+    {
+        $this->dashboard = $dashboard;
+        parent::__construct($env, $request);
+    }
+
     /**
      * @inheritDoc
      */
     public function isEnabled(): bool
     {
-        return true; // todo?
+        return $this->dashboard->isOnboardingCompleted() && current_user_can('manage_metricool');
     }
 
     /**
@@ -21,6 +32,6 @@ class UserSettingsLoader extends AbstractLoader
      */
     public function inScope(): bool
     {
-        return true; // todo?
+        return true;
     }
 }
