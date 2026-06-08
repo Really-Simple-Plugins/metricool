c475e48184f496095032ab1fad8dbb230eac968d
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2026-06-08 16:18:08 +0200

    feat: implemented newly split hook

--- react/src/components/custom/dashboard/analytics/tabs/RealtimeTab.tsx
+++ react/src/components/custom/dashboard/analytics/tabs/RealtimeTab.tsx
@@ -2,7 +2,7 @@ import { useGlobalContext } from "@/context/GlobalContext.tsx";
 import { __ } from "@wordpress/i18n";
 import { Button, FlexContainer, Icon, LineChart, LoadingAndErrorState } from "@/components/shared";
 import { MetricTile } from "@/components/custom/dashboard/analytics/MetricTile.tsx";
-import { useAnalyticsData } from "@/hooks/useAnalyticsData.tsx";
+import { useRealtimeAnalyticsData } from "@/hooks/analytics/useRealtimeAnalyticsData.tsx";
 
 const RealtimeTab = () => {
     const { metricoolDynamicUrl, metricool } = useGlobalContext();
@@ -16,7 +16,7 @@ const RealtimeTab = () => {
             refetch,
             errorUpdateCount
         }
-    } = useAnalyticsData({ tab: "realtime" });
+    } = useRealtimeAnalyticsData();
 
     const chartConfig = {
         pageViews: {
