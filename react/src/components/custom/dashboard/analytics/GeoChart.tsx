f571252c9e9ec49083fced82954f8fdafb9391be
willembraam <willem.braam@team.blue>
2026-06-09 10:26:03 +0200

    docs: moved inline comments to local variables for self-documenting purposes

--- react/src/components/custom/dashboard/analytics/GeoChart.tsx
+++ react/src/components/custom/dashboard/analytics/GeoChart.tsx
@@ -1,4 +1,4 @@
-import { Chart } from "react-google-charts";
+import {Chart} from "react-google-charts";
 
 type GeoChartProps = {
     chartData: string[][],
@@ -7,22 +7,29 @@ type GeoChartProps = {
 
 const GeoChart = ({ chartData, chartOptions }: GeoChartProps) => {
 
+    /**
+     * Google Chart requires the data array to always have headers,
+     * else it throws an error. It also requires the headers array
+     * to always have a length of 2 if there is no further data, or
+     * it throws a different error. Therefor we use this custom
+     * array if the backend returns an empty chartData array.
+     */
+    let fallbackChartData = [["", ""]];
+
+    /**
+     * Default chart version for Google Chart is 51, but if we do not set it
+     * explicitly, it will throw a warning in the console. Dont really know why.
+     */
+    let requiredChartVersion = "51";
+
     return (
         <Chart
-            /**
-             * Google Geochart requires the data array to always have headers,
-             * else it throws an error. It also requires the headers array
-             * to always have a length of 2 if there is no further data, or
-             * it throws a different error. Therefor we use this custom
-             * array if the backend returns an empty chartData array.
-             */
-            data={chartData.length > 0 ? chartData : [["", ""]]}
+            data={chartData.length > 0 ? chartData : fallbackChartData}
             chartType={"GeoChart"}
             options={chartOptions}
             height={"185px"}
             width={"100%"}
-            // chartVersion necessary to prevent console warning
-            chartVersion={"51"}
+            chartVersion={requiredChartVersion}
         />
     );
 };
